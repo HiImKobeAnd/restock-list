f8450bd06476795ccea8b3425b5a97cf7542a269
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-05-21 21:22:53 +0200

    Add sorting

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod products;
 
-use std::collections::HashMap;
-
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use askama::Template;
-
 use products::get_items;
+use products::{Product, Products};
 use serde::Deserialize;
+use std::collections::HashMap;
 
 #[derive(Deserialize, Debug)]
 struct Item {
-    value: i32,
+    #[serde(rename = "type")]
+    name: String,
+    quantity: i32,
 }
 
 #[get("/")]
@@ -19,30 +20,109 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body(items.render().unwrap())
 }
 
-#[post("/value_changed/{name}")]
-async fn value_changed(path: web::Path<String>, form: web::Form<Item>) -> impl Responder {
+#[post("/value_changed")]
+async fn value_changed(form: web::Form<Vec<Item>>) -> impl Responder {
     let items = get_items().products;
     let mut map = HashMap::new();
     for item in items {
         map.insert(item.name, item.required_amount);
     }
 
-    let name = path.into_inner();
-
-    let value = map.get(&name).unwrap();
+    let value = map.get(&form.first().unwrap().name).unwrap();
 
     HttpResponse::Ok().body(format!(
-        "<p class=\"table_number\" id=\"purchase_amount_{name}\">{}</p>",
-        *value as i32 - form.value as i32
+        "<p class=\"table_number\" id=\"purchase_amount_{}\">{}</p>",
+        form.first().unwrap().name,
+        *value as i32 - form.first().unwrap().quantity as i32
     ))
+}
+
+#[post("/sort_count")]
+async fn sort_count(form: web::Form<Vec<Item>>) -> impl Responder {
+    let items = get_items().products;
+    let mut vec = Vec::new();
+    for item in items.clone() {
+        vec.push((item.name, item.list_sort_number));
+    }
+    vec.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let mut form_map = HashMap::new();
+    for item in form.iter() {
+        form_map.insert(item.name.clone(), item.quantity);
+    }
+
+    let mut product_map = HashMap::new();
+    for item in items {
+        product_map.insert(item.name, item.required_amount);
+    }
+
+    let mut sorted_product = Vec::new();
+    for item in vec {
+        sorted_product.push(Product {
+            name: item.0.clone(),
+            start_value: *form_map.get(&item.0).unwrap(),
+            required_amount: *product_map.get(&item.0).unwrap(),
+            list_sort_number: 0,
+            restock_sort_number: 0,
+        });
+    }
+
+    let sorted_products = Products {
+        products: sorted_product,
+    };
+
+    HttpResponse::Ok().body(sorted_products.render().unwrap())
+}
+
+#[post("/sort_restock")]
+async fn sort_restock(form: web::Form<Vec<Item>>) -> impl Responder {
+    let items = get_items().products;
+    let mut vec = Vec::new();
+    for item in items.clone() {
+        vec.push((item.name, item.restock_sort_number));
+    }
+    vec.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let mut form_map = HashMap::new();
+    for item in form.iter() {
+        form_map.insert(item.name.clone(), item.quantity);
+    }
+
+    let mut product_map = HashMap::new();
+    for item in items {
+        product_map.insert(item.name, item.required_amount);
+    }
+
+    let mut sorted_product = Vec::new();
+    for item in vec {
+        sorted_product.push(Product {
+            name: item.0.clone(),
+            start_value: *form_map.get(&item.0).unwrap(),
+            required_amount: *product_map.get(&item.0).unwrap(),
+            list_sort_number: 0,
+            restock_sort_number: 0,
+        });
+    }
+
+    let sorted_products = Products {
+        products: sorted_product,
+    };
+
+    HttpResponse::Ok().body(sorted_products.render().unwrap())
 }
 
 #[actix_web::main]
 
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(value_changed))
-        .bind(("127.0.0.1", 8050))?
-        .workers(4)
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(value_changed)
+            .service(sort_count)
+            .service(sort_restock)
+    })
+    .bind(("127.0.0.1", 8050))?
+    .workers(4)
+    .run()
+    .await
 }
--- src/products.rs
+++ src/products.rs
@@ -9,8 +9,10 @@ pub struct Products {
 #[derive(Clone)]
 pub struct Product {
     pub name: String,
-    pub start_value: u16,
-    pub required_amount: u16,
+    pub start_value: i32,
+    pub required_amount: i32,
+    pub list_sort_number: i32,
+    pub restock_sort_number: i32,
 }
 
 pub fn get_items() -> Products {
@@ -18,106 +20,204 @@ pub fn get_items() -> Products {
         name: String::from("Kanelstang"),
         start_value: 5,
         required_amount: 8,
+        list_sort_number: 19,
+        restock_sort_number: 100,
     };
     let morgenstang = Product {
         name: String::from("Morgenstang"),
         start_value: 2,
         required_amount: 8,
+        list_sort_number: 18,
+        restock_sort_number: 100,
     };
     let brunsvigerstang = Product {
         name: String::from("Brunsvigerstang"),
         start_value: 7,
         required_amount: 8,
+        list_sort_number: 17,
+        restock_sort_number: 100,
     };
     let cremestang = Product {
         name: String::from("Cremestang"),
         start_value: 3,
         required_amount: 3,
+        list_sort_number: 25,
+        restock_sort_number: 100,
     };
     let kakaocroissaint = Product {
         name: String::from("Kakaocroissaint"),
         start_value: 3,
         required_amount: 3,
+        list_sort_number: 22,
+        restock_sort_number: 100,
     };
     let hoej_snegl = Product {
-        name: String::from("Høj snegl"),
+        name: String::from("Høj_snegl"),
         start_value: 4,
         required_amount: 8,
+        list_sort_number: 8,
+        restock_sort_number: 100,
     };
     let croissaint = Product {
         name: String::from("Crossaint"),
         start_value: 4,
         required_amount: 4,
+        list_sort_number: 23,
+        restock_sort_number: 100,
     };
     let chokolade_croissaint = Product {
-        name: String::from("Chokolade roissaint"),
+        name: String::from("Chokolade_croissaint"),
         start_value: 2,
         required_amount: 2,
+        list_sort_number: 21,
+        restock_sort_number: 100,
     };
     let kbh_birkes = Product {
-        name: String::from("KBH birkes"),
+        name: String::from("KBH_birkes"),
         start_value: 5,
         required_amount: 8,
+        list_sort_number: 7,
+        restock_sort_number: 100,
     };
     let birkes = Product {
         name: String::from("Birkes"),
         start_value: 3,
         required_amount: 4,
+        list_sort_number: 6,
+        restock_sort_number: 100,
     };
     let grovbirkes = Product {
         name: String::from("Grovbirkes"),
         start_value: 3,
         required_amount: 4,
+        list_sort_number: 5,
+        restock_sort_number: 100,
     };
     let onsdags_snegl = Product {
-        name: String::from("Onsdags snegl"),
+        name: String::from("Onsdags_snegl"),
         start_value: 3,
         required_amount: 10,
+        list_sort_number: 30,
+        restock_sort_number: 100,
     };
     let kanelsnurre = Product {
         name: String::from("Kanelsnurre"),
         start_value: 4,
         required_amount: 4,
+        list_sort_number: 15,
+        restock_sort_number: 100,
     };
     let snurre_med_remonce = Product {
-        name: String::from("Snurre med remonce"),
+        name: String::from("Snurre_med_remonce"),
         start_value: 4,
         required_amount: 4,
+        list_sort_number: 16,
+        restock_sort_number: 100,
     };
     let brunsviger_knude = Product {
-        name: String::from("Brunsviger knude"),
+        name: String::from("Brunsviger_knude"),
         start_value: 6,
         required_amount: 8,
+        list_sort_number: 24,
+        restock_sort_number: 100,
     };
     let skagenshorn = Product {
         name: String::from("Skagenshorn"),
         start_value: 2,
         required_amount: 2,
+        list_sort_number: 31,
+        restock_sort_number: 100,
     };
     let kaernemaelkshorn = Product {
         name: String::from("Kærnemælkshorn"),
         start_value: 2,
         required_amount: 2,
+        list_sort_number: 32,
+        restock_sort_number: 100,
     };
     let cremebolle = Product {
         name: String::from("Cremebolle"),
         start_value: 2,
         required_amount: 4,
+        list_sort_number: 33,
+        restock_sort_number: 100,
     };
     let choco_donut = Product {
-        name: String::from("Choco donut"),
+        name: String::from("Choco_donut"),
         start_value: 2,
         required_amount: 3,
+        list_sort_number: 3,
+        restock_sort_number: 100,
     };
     let rainbow_donut = Product {
-        name: String::from("Rainbow donut"),
+        name: String::from("Rainbow_donut"),
         start_value: 2,
         required_amount: 3,
+        list_sort_number: 2,
+        restock_sort_number: 100,
     };
     let pink_donut = Product {
-        name: String::from("Pink donut"),
+        name: String::from("Pink_donut"),
         start_value: 2,
         required_amount: 3,
+        list_sort_number: 1,
+        restock_sort_number: 100,
+    };
+    let macaron = Product {
+        name: String::from("Macaron"),
+        start_value: 3,
+        required_amount: 3,
+        list_sort_number: 4,
+        restock_sort_number: 100,
+    };
+    let stor_poelsehorn_med_ketchup = Product {
+        name: String::from("Stor_pølsehorn_med_ketchup"),
+        start_value: 4,
+        required_amount: 4,
+        list_sort_number: 11,
+        restock_sort_number: 100,
+    };
+    let stor_poelsehorn_med_kylling = Product {
+        name: String::from("Stor_pølsehorn_med_kylling"),
+        start_value: 4,
+        required_amount: 4,
+        list_sort_number: 9,
+        restock_sort_number: 100,
+    };
+    let lille_poelsehorn = Product {
+        name: String::from("Lille_pølsehorn"),
+        start_value: 4,
+        required_amount: 4,
+        list_sort_number: 10,
+        restock_sort_number: 100,
+    };
+    let pizzasnegl = Product {
+        name: String::from("Pizzasnegl"),
+        start_value: 4,
+        required_amount: 4,
+        list_sort_number: 14,
+        restock_sort_number: 100,
+    };
+    let pizza_pepperoni = Product {
+        name: String::from("Pizza_pepperoni"),
+        start_value: 3,
+        required_amount: 3,
+        list_sort_number: 12,
+        restock_sort_number: 100,
+    };
+    let pizza_donut = Product {
+        name: String::from("Pizza_donut"),
+        start_value: 4,
+        required_amount: 4,
+        list_sort_number: 13,
+        restock_sort_number: 100,
+    };
+    let wiener_kagemand_stang = Product {
+        name: String::from("Wiener_kagemand_stang"),
+        start_value: 3,
+        required_amount: 3,
+        list_sort_number: 20,
+        restock_sort_number: 100,
     };
     let products = Products {
         products: [
@@ -142,6 +242,14 @@ pub fn get_items() -> Products {
             birkes,
             kbh_birkes,
             chokolade_croissaint,
+            macaron,
+            stor_poelsehorn_med_ketchup,
+            stor_poelsehorn_med_kylling,
+            lille_poelsehorn,
+            pizzasnegl,
+            pizza_pepperoni,
+            pizza_donut,
+            wiener_kagemand_stang,
         ]
         .to_vec(),
     };
