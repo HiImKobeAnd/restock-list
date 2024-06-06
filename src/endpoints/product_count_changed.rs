1a244d14cd1ea1c89ba9a5cc0307b87f1e332538
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-06-06 14:18:57 +0200

    Use global HashMap instead of creating a new on every function call

--- src/endpoints/product_count_changed.rs
+++ src/endpoints/product_count_changed.rs
@@ -1,23 +1,21 @@
 use crate::data::{AppData, Item};
 use actix_web::{post, web, HttpResponse, Responder};
-use std::collections::HashMap;
+use std::borrow::Borrow;
 
 #[post("/product_count_changed")]
 pub async fn product_count_changed(
     form: web::Form<Vec<Item>>,
     data: web::Data<AppData>,
 ) -> impl Responder {
-    let items = (*data.into_inner()).products.clone().products;
-    let mut map = HashMap::new();
-    for item in items {
-        map.insert(item.name, (item.required_amount, item.daily_use));
-    }
+    let data = data.borrow();
+    let map = data.products_map.clone();
 
-    let value = map.get(&form.first().unwrap().name).unwrap();
+    let product = map.get(&form.first().unwrap().name).unwrap();
 
     HttpResponse::Ok().body(format!(
         "<p class=\"table_number\" id=\"purchase_amount_{}\">{}</p>",
         form.first().unwrap().name,
-        (((value.1 * 2) + value.0) - form.first().unwrap().quantity).clamp(0, 10) // The clamp will be problematic if the product's tri-daily restock needs exceed 10
+        (((product.required_amount * 2) + product.daily_use) - form.first().unwrap().quantity)
+            .clamp(0, product.required_amount + product.daily_use)
     ))
 }
