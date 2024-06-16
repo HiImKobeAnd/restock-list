d764793568a7b49e1af9b72cc8ae8333c164f8fe
HiImKobeAnd <HiImKobeAnd@gmail.com>
2024-06-16 17:36:10 +0200

    Update sort function

--- src/endpoints/sort.rs
+++ src/endpoints/sort.rs
@@ -30,40 +30,42 @@ pub async fn sort(
 ) -> impl Responder {
     let products_map = data.products_map.clone();
     let products = data.products.products.clone();
-    let sort_method_string = path.into_inner();
 
-    let sort_method =
-        SortMethod::try_from(sort_method_string.as_str()).unwrap_or(SortMethod::Shelf);
+    let sort_method = SortMethod::try_from(path.into_inner().as_str()).unwrap_or(SortMethod::Shelf);
 
-    let mut vec = Vec::new();
-    for product in products.clone() {
+    let mut unsorted_products = Vec::new();
+    for product in products {
         match sort_method {
-            SortMethod::Restock => vec.push((product.name, product.restock_sort_number)),
-            SortMethod::Shelf => vec.push((product.name, product.shelf_sort_number)),
+            SortMethod::Restock => {
+                unsorted_products.push((product.name, product.restock_sort_number))
+            }
+            SortMethod::Shelf => unsorted_products.push((product.name, product.shelf_sort_number)),
         }
     }
 
-    vec.sort_by(|a, b| a.1.cmp(&b.1));
+    unsorted_products.sort_by(|a, b| a.1.cmp(&b.1));
 
     let mut form_map = HashMap::new();
     for product in form.iter() {
         form_map.insert(product.name.clone(), product.quantity);
     }
 
-    let mut sorted_product = Vec::new();
-    for product in vec {
-        sorted_product.push(Product {
-            name: product.0.clone(),
-            start_value: *form_map.get(&product.0).unwrap(),
-            required_amount: products_map.get(&product.0).unwrap().required_amount,
-            shelf_sort_number: 0,
-            restock_sort_number: 0,
-            daily_use: 0,
+    let mut sorted_products = Vec::new();
+    for product in unsorted_products {
+        let product_name = product.0;
+        let product = products_map.get(&product_name).unwrap();
+        sorted_products.push(Product {
+            name: product_name.clone(),
+            start_value: *form_map.get(&product_name).unwrap(),
+            required_amount: product.required_amount,
+            shelf_sort_number: product.shelf_sort_number,
+            restock_sort_number: product.restock_sort_number,
+            daily_use: product.daily_use,
         });
     }
 
     let sorted_products = Products {
-        products: sorted_product,
+        products: sorted_products,
     };
 
     HttpResponse::Ok().body(sorted_products.render().unwrap())
