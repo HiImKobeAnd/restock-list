61412bb9a544bd64f5923f9ea30b0e7f8f65411c
hiimkobeand <hiimkobeand@gmail.com>
2025-04-11 11:39:37 +0200

    build: Add flake.nix development environment

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        libraries = with pkgs; [
+        ];
+        packages = with pkgs; [
+          # rust
+          rust-analyzer
+          rustfmt
+          clippy
+          rustc
+          cargo
+          cargo-deny
+          cargo-edit
+          cargo-watch
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages ++ [
+            (
+              # Needed for rust-analyzer
+              pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              }
+            )
+          ];
+
+          # Needed for rust-analyzer
+          RUST_SRC_PATH = "${
+            pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            }
+          }/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
