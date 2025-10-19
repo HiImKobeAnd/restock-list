20d136bc24071f3064dd07ee7c6643304c2a08a1
hiimkobeand <hiimkobeand@gmail.com>
2025-10-19 11:30:09 +0200

    fix: Fix install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-cargo build --release
-cp ./target/release/restock_list /usr/local/bin
+cp ./target/release/restock_list /opt/restock_list
+cp ./products.yaml /opt/restock_list
+cp ./configuration.yaml /opt/restock_list
 cp ./restock-list.service /etc/systemd/system
+ln -s /opt/restock_list/restock_list /usr/local/bin/restock_list
